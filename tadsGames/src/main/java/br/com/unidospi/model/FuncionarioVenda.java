a6712d165ee019359c7b72e5e8701731245511c0
gabrielgoncalves2008@msn.com <>
2019-10-10 19:53:14 -0300

    Classes Funcionarios

---
+++ tadsGames/src/main/java/br/com/unidospi/model/FuncionarioVenda.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.unidospi.model;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author gabri
+ */
+public class FuncionarioVenda extends Funcionario {
+
+    public FuncionarioVenda(double salario, String departamento, String cargo, 
+            String login, String senha, int id, String nome, String cpf, 
+            Date dataNasc, boolean ativo) {
+        super(salario, departamento, cargo, login, senha, id, nome, cpf, dataNasc, ativo);
+    }
+
+    public FuncionarioVenda(double salario, String departamento, 
+            String cargo, String login, String senha, String nome, String cpf, 
+            Date dataNasc, boolean ativo) {
+        super(salario, departamento, cargo, login, senha, nome, cpf, dataNasc, ativo);
+    }
+    
+    public void mostrarFaturamento() {}
+    public void criarVenda() {}
+}
