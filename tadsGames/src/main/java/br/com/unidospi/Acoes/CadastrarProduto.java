a47d5bff7675a63670a2231b1bff60dfaa88264a
lucas-sdf@hotmail.com.br <lucas@Lucas-PC>
2019-10-29 18:39:26 -0200

    Validacoes

--- tadsGames/src/main/java/br/com/unidospi/Acoes/CadastrarProduto.java
+++ tadsGames/src/main/java/br/com/unidospi/Acoes/CadastrarProduto.java
@@ -10,8 +10,6 @@ import br.com.unidospi.DAO.ProdutoDAO;
 import br.com.unidospi.model.Produto;
 import java.io.IOException;
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
