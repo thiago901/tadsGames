257900c7e6d20bbf387ec7b63e29279f105c08ac
Gabriel <11437182+gisidorio@users.noreply.github.com>
2019-11-09 01:30:59 -0300

    Delete CriarUsuarios.sql

--- CriarUsuarios.sql
+++
@@ -1,7 +0,0 @@
-USE dbGames;
-
-INSERT INTO Usuario (nomeUsuario, senha, dataCadastro, ativo, idFuncionario) 
-VALUES ('mazinhacardoso', '948571', curdate(), TRUE, 1),
-('mmarinho', '395678', curdate(), TRUE, 3),
-('araujovitor', '908070', curdate(), TRUE, 2),
-('valeriaAlves', '905941', curdate(), FALSE, 4);
