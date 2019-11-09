853666e7ad53f64fa6c67cee71c8ea7843a50ce3
Gabriel <11437182+gisidorio@users.noreply.github.com>
2019-11-09 01:32:23 -0300

    Add files via upload

---
+++ CriarUsuarios.sql
@@ -0,0 +1,8 @@
+USE dbGames;
+
+INSERT INTO Usuario (nomeUsuario, senha, dataCadastro, ativo, idFuncionario) 
+VALUES ('mazinhacardoso', '948571', curdate(), TRUE, 1),
+('mmarinho', '395678', curdate(), TRUE, 3),
+('araujovitor', '908070', curdate(), TRUE, 2),
+('valeriaAlves', '905941', curdate(), FALSE, 4),
+('bebezinha', '12345', curdate(), TRUE, 7);
