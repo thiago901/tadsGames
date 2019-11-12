c6dcbb07fb7ce91157513f0af4d233026762df6c
Gabriel <11437182+gisidorio@users.noreply.github.com>
2019-11-12 17:50:22 -0300

    Update CriarFuncionarios.sql

--- CriarFuncionarios.sql
+++ CriarFuncionarios.sql
@@ -4,8 +4,8 @@ INSERT INTO Funcionario (idEmpresa, nomeFuncionario, sobrenome, sexo, cpf, dtNas
 (1, 'Maria', 'Cardoso', 'Feminino', '99950493849', '1970-02-12', true, '2000', 'Gerente Regional', 'Vendas'),
 (4, 'Vitor', 'Araujo', 'Masculino', '88889593849', '1979-07-20', true, '10000', 'Diretor', 'Retaguarda'),
 (2, 'Marcos', 'Marinho', 'Masculino', '77793793849', '1980-01-10', true, '8500', 'Gerente Global', 'TI'),
-(1, 'Valeria', 'Alves', 'Feminino', '23593793849', '1985-01-10', false, '3500', 'Funcionária RH', 'Administrativo'),
+(1, 'Valeria', 'Alves', 'Feminino', '23593793849', '1985-01-10', false, '3500', 'Funcionário RH', 'Administrativo'),
 (3, 'Alessandra', 'Santana', 'Feminino', '70943793849', '1980-07-20', true, '6000', 'Gerente Regional', 'TI'),
-(2, 'Jandira', 'Santos', 'Feminino', '19437938491', '1980-07-22', true, '15500', 'Diretora', 'TI'), 
-(2, 'Bebezinha', 'Ya', 'Feminino', '95847382910', '1988-07-22', true, '15500', 'Diretora', 'TI');
+(2, 'Jandira', 'Santos', 'Feminino', '19437938491', '1980-07-22', true, '15500', 'Diretor', 'TI'), 
+(2, 'Bebezinha', 'Ya', 'Feminino', '95847382910', '1988-07-22', true, '15500', 'Diretor', 'TI');
 
