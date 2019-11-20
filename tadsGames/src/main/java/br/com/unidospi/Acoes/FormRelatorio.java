def16a7f1255ffcb8fbd8c85ac4bb3c043a5583d
lucas <lucas@Lucas-PC>
2019-11-20 14:53:43 -0200

    Autorização relatorio e formRelatorio

--- tadsGames/src/main/java/br/com/unidospi/Acoes/FormRelatorio.java
+++ tadsGames/src/main/java/br/com/unidospi/Acoes/FormRelatorio.java
@@ -5,13 +5,7 @@
  */
 package br.com.unidospi.Acoes;
 
-import br.com.unidospi.Controller.RelatoriosController;
-import br.com.unidospi.DAO.RelatorioDao;
-import br.com.unidospi.model.RelatorioFaturaDia;
-import br.com.unidospi.model.RelatorioPercentagem;
-import br.com.unidospi.model.RelatorioTop10;
 import java.io.IOException;
-import java.util.ArrayList;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -26,7 +20,7 @@ public class FormRelatorio implements Executavel{
     @Override
     public String executa(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
                 
-        RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/jsp/cliente/Relatorio.jsp");
+        RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/jsp/Relatorio/Relatorio.jsp");
         rd.forward(req, resp);
         return "";
     }
