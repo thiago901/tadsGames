ba170289a6b182935dd095420e2497d302328863
tsrocha901@gmail.com <>
2019-11-23 18:05:12 -0200

    Correcoes Telas de venda

--- tadsGames/src/main/java/br/com/unidospi/util/main.java
+++ tadsGames/src/main/java/br/com/unidospi/util/main.java
@@ -7,17 +7,25 @@ package br.com.unidospi.util;
 
 import com.mysql.cj.x.protobuf.MysqlxPrepare.Execute;
 import java.io.IOException;
+import java.util.Set;
+import javax.servlet.ServletContainerInitializer;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 
 /**
  *
  * @author HENRIQUE BASTOS
  */
-public class main {
+public class main implements ServletContainerInitializer{
        
     public static void main(String[] args) {
         
        
         
+    }
+
+    @Override
+    public void onStartup(Set<Class<?>> c, ServletContext ctx) throws ServletException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
