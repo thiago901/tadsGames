5f0282603225ddd4d5a8b6e81f84546d0e4a8dad
HENRIQUE BASTOS <HENRIQUE BASTOS@10.0.0.125>
2019-11-23 10:28:09 -0200

    Add gerar jar com dependencia

---
+++ tadsGames/src/main/java/br/com/unidospi/util/main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.unidospi.util;
+
+import com.mysql.cj.x.protobuf.MysqlxPrepare.Execute;
+import java.io.IOException;
+import javax.servlet.ServletException;
+
+/**
+ *
+ * @author HENRIQUE BASTOS
+ */
+public class main {
+       
+    public static void main(String[] args) {
+        
+       
+        
+    }
+}
